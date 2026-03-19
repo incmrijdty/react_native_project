0dc6bc3d8cfa158e80e81d5b3d0c4a0206a3787e
incmrijdty <alesia.sichova@yandex.by>
2026-03-19 18:16:33 +0100

    set up redux store

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function Dashboard() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Expense Tracker</Text>
+      <Text>Expense Tracker, redux ready</Text>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,17 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
+import { Provider } from "react-redux";
+import { store } from "../src/store/store";
 
 export default function RootLayout() {
   return (
-    <>
+    <Provider store={store}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
 
       <StatusBar style="auto" />
-    </>
+    </Provider>
   );
 }
